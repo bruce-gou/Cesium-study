eda771351f0a1023214c6e21bd57862a3c177a38
苟金贵 <2829960738@qq.com>
2021-01-28 09:34:07 +0800

    feat: 修改模型

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
       <Row gutter={10}>
         <Col span={3}><Link to="/plane">飞机飞行demo</Link></Col>
         <Col span={3}><Link to="/routePlanning">路线规划demo</Link></Col>
+        <Col span={3}><Link to="/threeDtiles">加载3dtiles</Link></Col>
       </Row>
     </div>
   );
--- demo/src/router/index.js
+++ demo/src/router/index.js
@@ -4,6 +4,7 @@ import App from '../App.jsx';
 // 二级页面
 import Plane from '../page/plane.jsx';
 import RoutePlanning from '../page/routePlanning';
+import ThreeDtiles from '../page/threedtiles';
 
 
 import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
@@ -18,6 +19,8 @@ const Routers = () => {
         <Route exact path="/" component={App} />
         <Route exact path="/plane" component={Plane}></Route>
         <Route exact path="/routePlanning" component={RoutePlanning}></Route>
+        <Route exact path="/threeDtiles" component={ThreeDtiles}></Route>
+
         {/* <Home>
             <Route exact path="/sys/user" component={User} />
           </Home> */}
